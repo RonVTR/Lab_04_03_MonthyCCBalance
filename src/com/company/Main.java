2f131ebf61764113b83f40a6aa36d296c70a7da0
aaronvictor <aaron.victor30@gmail.com>
2022-06-05 23:33:17 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int currentBalance = 5000;
+
+        System.out.println(currentBalance * .17 + " is your your intrest for one month");
+        System.out.println(currentBalance * .17 * 2 + " is your your intrest for two month");
+    }
+}
